38e696f2f2131921224db0dd65e0197f3c3cf110
Abdur Rehman Hashmi <hashmi633@gmail.com>
2025-05-05 11:45:01 +0500

    function tool added

--- openai-agent-sdk/projects/weather-assistant/main.py
+++ openai-agent-sdk/projects/weather-assistant/main.py
@@ -1,6 +1,54 @@
 import os
+from agents import AsyncOpenAI,Agent, OpenAIChatCompletionsModel, RunConfig, Runner, ModelProvider
 from dotenv import load_dotenv
+from weather_tool import get_weather
+import asyncio
+from typing import cast
 
 load_dotenv()
-WEATHER_API_KEY = os.getenv("OPENWEATHERMAP_API_KEY")
+
 GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+
+external_client = AsyncOpenAI(
+    api_key = GEMINI_API_KEY,
+    base_url= "https://generativelanguage.googleapis.com/v1beta/openai/",
+)
+
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash",
+    openai_client= external_client,
+)
+
+config = RunConfig(
+    model=model,
+    model_provider= cast(ModelProvider, external_client),
+    tracing_disabled=True,
+)
+
+weather_assistant = Agent(
+    name="Weather Assistant",
+    instructions="""You are a weather assistant that can provide current weather information.
+  
+   When asked about weather, use the get_weather tool to fetch accurate data.
+   If the user doesn't specify a country code and there might be ambiguity,
+   ask for clarification (e.g., Paris, France vs. Paris, Texas).
+  
+   Provide friendly commentary along with the weather data, such as clothing suggestions
+   or activity recommendations based on the conditions.
+   """,
+   tools=[get_weather],
+)
+
+async def main():
+    try:
+        result = await Runner.run(
+            weather_assistant,
+            "what's the weather like in newyork, USA",
+            run_config=config,
+        )
+        print(result.final_output)
+    except Exception as e:
+        return f"Error: {str(e)}"
+    
+if __name__ == "__main__":
+    asyncio.run(main())
